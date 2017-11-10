a0bab708a48dba916fb76ae4069096957368a7e6
李贤 <xianguo@maihaoche.com>
2017-11-10 16:46:06 +0800

    Initial commit

---
+++ aomo-common/src/main/java/com/aomo/Main.java
@@ -0,0 +1,12 @@
+package com.aomo;
+
+/**
+ * @author XianGuo
+ * @Date 2017-11-10
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("测试方法");
+    }
+}
